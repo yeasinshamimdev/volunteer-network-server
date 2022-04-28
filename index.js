8d5fe8f462a483331687e584e9c0d26df33be71c
Shamim <yeasinshamim7@gmail.com>
2022-04-28 17:46:42 +0600

    heroku server added

--- index.js
+++ index.js
@@ -12,12 +12,33 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2rwge.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('db connected');
-    //   client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const volunteersCollection = client.db("volunteers").collection("volunteer");
+        const volunteersServices = client.db("volunteerServices").collection("service");
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = volunteersServices.find(query);
+
+            if ((await cursor.count() === 0)) return res.send({ succuss: "no data found", message: "Please insert data then request for data!!" });
+
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.post('/addservice', async (req, res) => {
+            const data = req.body;
+            const result = await volunteersServices.insertOne(data);
+            res.send(result);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('volunteer network server running');
